45762f209438514a40fc1e664a3e34bb8dff59af
Akash kumar <ak.akki907@gmail.com>
2024-03-30 11:14:44 +0530

    initial commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import StreamVideoProvider from '@/providers/StreamVideoProvider';
+import { ReactNode } from 'react';
+
+// import StreamVideoProvider from '@/providers/StreamClientProvider';
+
+const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
+};
+
+export default RootLayout;
